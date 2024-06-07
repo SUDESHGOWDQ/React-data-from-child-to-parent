602884c618e7fc413135e150941d1c0bcd952efb
unknown <sudeshmd1997@gmail.com>
2024-06-07 11:49:41 +0530

    mama

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-rafce
+
+import React, { useState } from 'react';
+import ChildComponent from './Child/Form';
+
+const App = () => {
+  const [formData, setFormData] = useState('');
+
+  
+  const handleFormSubmit = (data) => {
+    setFormData(data);
+  };
+
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      <p>Form Data: {formData}</p>
+      <ChildComponent handleFormSubmit={handleFormSubmit} />
+    </div>
+  );
+};
+
+export default App;
--- src/Child/Form.js
+++ src/Child/Form.js
@@ -1,1 +1,24 @@
-rafce
+
+import React, { useState } from 'react';
+
+const Form = ({ handleFormSubmit }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();  
+    handleFormSubmit(inputValue);
+    setInputValue(''); }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
